824e9d29bf74387a2e511e78619e217b414648ce
RafaelNercessian <rafanercessian@gmail.com>
2024-06-28 18:13:20 -0600

    New changes to hero

--- wp-content/themes/inet_lightspeed-child/partials/hero.php
+++ wp-content/themes/inet_lightspeed-child/partials/hero.php
@@ -10,11 +10,18 @@ $backgroundHeroId = esc_html(get_field('background_hero')['id']);
         <h4 class="hero__subtitle"><?= $subtitle ?></h4>
     </section>
     <section class="col-lg-4 hero__containerimage">
-        <section class="row">
-            <div class="hero__imagebackground">
-                <?= wp_get_attachment_image($backgroundHeroId, 'large') ?>
-            </div>
-            <?= wp_get_attachment_image($wineImageId, 'large') ?>
+        <section class="hero__imagebackground">
+            <?= wp_get_attachment_image($backgroundHeroId, 'large') ?>
+        </section>
+        <section class="row d-flex align-items-end">
+            <section class="col-lg-10">
+                <?= wp_get_attachment_image($wineImageId, 'large') ?>
+            </section>
+            <section class="col-lg-2 hero__icon d-flex gap-1">
+                <i class="icon-twitter hero__icon--white"></i>
+                <i class="icon-instagram hero__icon--white"></i>
+                <i class="icon-facebook hero__icon--white"></i>
+            </section>
         </section>
     </section>
 </section>
