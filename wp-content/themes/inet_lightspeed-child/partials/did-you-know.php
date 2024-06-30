8b111c325c5cfd1bdf1d52112417396483f977cc
RafaelNercessian <rafanercessian@gmail.com>
2024-06-29 20:24:22 -0600

    Changes to owl carousel

--- wp-content/themes/inet_lightspeed-child/partials/did-you-know.php
+++ wp-content/themes/inet_lightspeed-child/partials/did-you-know.php
@@ -5,7 +5,7 @@ $content = esc_html(get_sub_field('content'));
 ?>
 <section class="didyouknow">
     <section class="row container mx-auto">
-        <section class="col-12">
+        <section class="col-12 mb-4">
             <h2 class="didyouknow__title"><?= $title ?></h2>
             <h3 class="didyouknow__subtitle"><?= $subtitle ?></h3>
             <p class="didyouknow__content h6"><?= $content ?></p>
@@ -23,12 +23,17 @@ $content = esc_html(get_sub_field('content'));
                 $featured_query = new WP_Query($args);
                 if ($featured_query->have_posts()) : ?>
                     <?php while ($featured_query->have_posts()) : $featured_query->the_post(); ?>
-                        <div class="carousel-item">
-                            <?php if (has_post_thumbnail()) : ?>
-                                <?php the_post_thumbnail('large'); ?>
-                            <?php endif; ?>
-                            <h6 class="carousel__title text-uppercase fw-normal my-2"><?php the_title(); ?></h6>
-                        </div>
+                        <a class="achorcarousel" href="<?php the_permalink() ?>">
+                            <div class="carousel-item">
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <div class="carousel-image-container">
+                                        <?php the_post_thumbnail('large'); ?>
+                                        <div class="carousel-overlay"></div>
+                                        <h3 class="carousel__title fw-normal"><?php the_title(); ?></h3>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </a>
                     <?php endwhile;
                 endif; ?>
             </div>
