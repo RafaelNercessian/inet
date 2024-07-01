523683a1c96ae010854f10c5a8193ab21c17524f
RafaelNercessian <rafanercessian@gmail.com>
2024-07-01 09:54:17 -0600

    Changes to footer

--- wp-content/themes/inet_lightspeed-child/footer.php
+++ wp-content/themes/inet_lightspeed-child/footer.php
@@ -4,63 +4,70 @@
  */
 ?>
 
-<footer id="primary-footer" class="container-fluid px-md-0 pt-3 footer">
-    <section class="row container mx-auto px-md-0 footer__wrapper">
-        <div class="col-lg-12 px-md-0">
-            <?= wp_get_attachment_image('112', 'large') ?>
-        </div>
-        <div class="col-lg-11">
-
-        </div>
-        <div class="row footer__linkscontainer">
-            <div class="col-lg-2">
-                <h4 class="footer__title text-uppercase">Our products</h4>
-                <nav class="footer-navigation">
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer-menu',
-                        'menu_class' => 'footer-menu',
-                        'container' => false,
-                    ));
-                    ?>
-                </nav>
-            </div>
-            <div class="col-lg-2">
-                <h4 class="footer__title text-uppercase">Our company</h4>
-                <nav class="footer-navigation">
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer-menu-two',
-                        'menu_class' => 'footer-menu-two',
-                        'container' => false,
-                    ));
-                    ?>
-                </nav>
-            </div>
-            <div class="col-lg-2">
-                <h4 class="footer__title text-uppercase">Get in touch</h4>
-                <a class="footer__phone" href="tel:+14034661288">403-466-1288</a>
-                <a class="footer__email" href="mailto:email@fccellars.com">email@fccellars.com</a>
-                <div class="footer__socials mt-2 d-flex">
-                    <i class="icon-twitter"></i>
-                    <i class="icon-instagram ms-1"></i>
-                    <i class="icon-facebook ms-1"></i>
+<footer id="primary-footer" class="container-fluid pt-3 footer">
+    <section class="container mx-auto footer__wrapper">
+        <div class="footer_test row">
+            <div class="d-flex align-items-center flex-column flex-xxl-row">
+                <div class="">
+                    <?php the_custom_logo() ?>
+                </div>
+                <div class="">
+                    <p class="footer__companyname ms-1">Cork+Vine Cellars</p>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <h4 class="footer__title text-uppercase">Join Our Team</h4>
-                <p class="footer__jointeam">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-                    porttitor
-                    non dolor a placerat.</p>
-                <a href="#" class="d-flex gap-1 align-items-center fw-bold text-uppercase footer__anchorlink mt-1">View
-                    Contact Us<i
-                            class="icon-long-arrow-right"></i></a>
+        </div>
+        <div class="row">
+            <div class="row footer__linkscontainer">
+                <div class="col-12 col-lg-4 col-xxl-2">
+                    <h4 class="footer__title text-uppercase">Our products</h4>
+                    <nav class="footer-navigation text-uppercase">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'footer-menu',
+                            'menu_class' => 'footer-menu',
+                            'container' => false,
+                        ));
+                        ?>
+                    </nav>
+                </div>
+                <div class="col-12 col-lg-4 col-xxl-2">
+                    <h4 class="footer__title text-uppercase fw-bold">Our company</h4>
+                    <nav class="footer-navigation  text-uppercase">
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'footer-menu-two',
+                            'menu_class' => 'footer-menu-two',
+                            'container' => false,
+                        ));
+                        ?>
+                    </nav>
+                </div>
+                <div class="col-12 col-lg-4 col-xxl-2">
+                    <h4 class="footer__title text-uppercase fw-bold">Get in touch</h4>
+                    <a class="footer__phone d-block" href="tel:+14034661288">403-466-1288</a>
+                    <a class="footer__email d-block" href="mailto:email@fccellars.com">email@fccellars.com</a>
+                    <div class="footer__socials mt-2 d-flex">
+                        <i class="icon-twitter"></i>
+                        <i class="icon-instagram ms-1"></i>
+                        <i class="icon-facebook ms-1"></i>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-12 col-xxl-4 mt-4 mt-xxl-0">
+                    <h4 class="footer__title text-uppercase fw-bold">Join Our Team</h4>
+                    <p class="footer__jointeam">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                        porttitor
+                        non dolor a placerat.</p>
+                    <a href="#" class="d-flex gap-1 align-items-center fw-bold text-uppercase footer__anchorlink mt-1">View
+                        Contact Us<i
+                                class="icon-long-arrow-right"></i></a>
+                </div>
             </div>
         </div>
     </section>
     <section class="container px-md-0 d-flex justify-content-between footer__disclaimer">
         <div>
-            <p class="footer__copyright mb-0">&copy; <?= date('Y') ?> Cork + Vine Cellars | Web design & development by iNet
+            <p class="footer__copyright mb-0">&copy; <?= date('Y') ?> Cork + Vine Cellars | Web design & development by
+                iNet
                 Media Ltd.
             <p></p>
         </div>
