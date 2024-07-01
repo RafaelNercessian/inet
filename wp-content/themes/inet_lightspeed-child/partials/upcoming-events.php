b08889368136154f56794bdbe1c42096d00e1248
RafaelNercessian <rafanercessian@gmail.com>
2024-06-30 20:52:28 -0600

    changes events

--- wp-content/themes/inet_lightspeed-child/partials/upcoming-events.php
+++ wp-content/themes/inet_lightspeed-child/partials/upcoming-events.php
@@ -21,15 +21,26 @@ function processDate($dateString)
 ?>
 <section class="events__backgroundimage">
     <section class="events__wrapper">
-        <section class="container d-flex align-items-start row mx-auto justify-content-between">
+        <section class="container d-flex align-items-md-center row mx-auto justify-content-between">
             <section class="col-lg-6">
-                <h2 class="welcome__title"><?= $title ?></h2>
-                <h3 class="welcome__subtitle fw-normal"><?= $subtitle ?></h3>
-                <p class="h6 welcome__content my-1"><?= $content ?></p>
-                <a href="#" class="d-flex gap-1 align-items-center fw-bold text-uppercase welcome__anchorlink">Our story<i
-                            class="icon-long-arrow-right"></i></a>
+                <h2 class="events__title"><?= $title ?></h2>
+                <h3 class="events__subtitle fw-normal"><?= $subtitle ?></h3>
+                <p class="events__content"><?= $content ?></p>
+                <div class="row w-100">
+                    <div class="col-md-12 col-lg-6">
+                        <a href="#" class="d-flex gap-1 align-items-center fw-bold text-uppercase welcome__anchorlink">View
+                            All Upcoming Events<i
+                                    class="icon-long-arrow-right"></i></a>
+                    </div>
+                    <div class="col-md-12 col-lg-6">
+                        <a href="#" class="d-flex gap-1 align-items-center fw-bold text-uppercase welcome__anchorlink">Book
+                            a private event<i
+                                    class="icon-long-arrow-right"></i></a>
+                    </div>
+                </div>
+
             </section>
-            <section class="col-lg-5">
+            <section class="col-lg-5 events__list">
                 <?php
                 if ($repeaterRows):
                     foreach ($repeaterRows as $id => $row):
@@ -44,12 +55,12 @@ function processDate($dateString)
                                 if (empty($dateTwo)): ?>
                                     <div class="d-flex align-items-start mt-2 ml-1 gap-1">
                                         <i class="icon-date"></i>
-                                        <p class="mb-0 h6"><?= $row['date']; ?></p>
+                                        <p class="mb-0 event__date"><?= $row['date']; ?></p>
                                     </div>
                                 <?php elseif ($dateOne['month'] == $dateTwo['month'] && $dateOne['year'] == $dateTwo['year']): ?>
                                     <div class="d-flex align-items-start mt-2 ml-1 gap-1">
                                         <i class="icon-date"></i>
-                                        <p class="mb-0 h6">
+                                        <p class="mb-0 event__date--format">
                                             <?= $dateOne['monthName'] . ' ' . $dateOne['day'] . '-' . $dateTwo['day'] . ', ' . $dateOne['year']; ?>
                                         </p>
                                     </div>
@@ -57,14 +68,14 @@ function processDate($dateString)
                             endif; ?>
                             <div class="d-flex align-items-start mt-1 gap-1">
                                 <i class="icon-map-pin"></i>
-                                <p class="mb-0 h6"><?= esc_html($row['address']); ?></p>
+                                <p class="mb-0 event__address"><?= esc_html($row['address']); ?></p>
                             </div>
                             <div class="d-flex align-items-start mt-1 gap-1">
                                 <i class="icon-time"></i>
                                 <div>
-                                    <p class="mb-0 h6"><?= esc_html($dateOne['dayOfWeek']) . ' ' . str_replace(' ', '', esc_html($row['time_event_starts'])) . ' - ' . str_replace(' ', '', esc_html($row['time_event_ends'])); ?></p>
+                                    <p class="mb-0  time__eventone"><?= esc_html($dateOne['dayOfWeek']) . ' ' . str_replace(' ', '', esc_html($row['time_event_starts'])) . ' - ' . str_replace(' ', '', esc_html($row['time_event_ends'])); ?></p>
                                     <?php if (!empty($row['time_event_starts_day_two']) && !empty($row['time_event_ends_day_two'])): ?>
-                                        <p class="mb-0 h6 time__eventtwo"><?= esc_html($dateTwo['dayOfWeek']) . ' ' . str_replace(' ', '', esc_html($row['time_event_starts_day_two'])) . ' - ' . str_replace(' ', '', esc_html($row['time_event_ends_day_two'])); ?></p>
+                                        <p class="mb-0 mt-0 time__eventtwo"><?= esc_html($dateTwo['dayOfWeek']) . ' ' . str_replace(' ', '', esc_html($row['time_event_starts_day_two'])) . ' - ' . str_replace(' ', '', esc_html($row['time_event_ends_day_two'])); ?></p>
                                     <?php endif; ?>
                                 </div>
                             </div>
