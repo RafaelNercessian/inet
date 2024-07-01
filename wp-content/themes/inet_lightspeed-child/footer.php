a8397dfe18390be22ad47bb0632cfcc8d7fe739d
RafaelNercessian <rafanercessian@gmail.com>
2024-07-01 08:06:45 -0600

    Changes to footer

--- wp-content/themes/inet_lightspeed-child/footer.php
+++ wp-content/themes/inet_lightspeed-child/footer.php
@@ -4,9 +4,9 @@
  */
 ?>
 
-<footer id="primary-footer" class="container-fluid px-0 pt-3 footer">
-    <section class="row container mx-auto px-0">
-        <div class="col-lg-12">
+<footer id="primary-footer" class="container-fluid px-md-0 pt-3 footer">
+    <section class="row container mx-auto px-md-0 footer__wrapper">
+        <div class="col-lg-12 px-md-0">
             <?= wp_get_attachment_image('112', 'large') ?>
         </div>
         <div class="col-lg-11">
@@ -39,10 +39,33 @@
             </div>
             <div class="col-lg-2">
                 <h4 class="footer__title text-uppercase">Get in touch</h4>
+                <a class="footer__phone" href="tel:+14034661288">403-466-1288</a>
+                <a class="footer__email" href="mailto:email@fccellars.com">email@fccellars.com</a>
+                <div class="footer__socials mt-2 d-flex">
+                    <i class="icon-twitter"></i>
+                    <i class="icon-instagram ms-1"></i>
+                    <i class="icon-facebook ms-1"></i>
+                </div>
             </div>
             <div class="col-lg-4">
                 <h4 class="footer__title text-uppercase">Join Our Team</h4>
+                <p class="footer__jointeam">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                    porttitor
+                    non dolor a placerat.</p>
+                <a href="#" class="d-flex gap-1 align-items-center fw-bold text-uppercase footer__anchorlink mt-1">View
+                    Contact Us<i
+                            class="icon-long-arrow-right"></i></a>
             </div>
+        </div>
+    </section>
+    <section class="container px-md-0 d-flex justify-content-between footer__disclaimer">
+        <div>
+            <p class="footer__copyright mb-0">&copy; <?= date('Y') ?> Cork + Vine Cellars | Web design & development by iNet
+                Media Ltd.
+            <p></p>
+        </div>
+        <div>
+            <a class="footer__privacy" href="#">Privacy Policy</a>
         </div>
     </section>
 </footer>
